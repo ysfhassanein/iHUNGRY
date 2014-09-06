d772b4f13cb8d3e04e0ba67f94825d196e4b6147
Youssif Hassanein <ysfhassanein@gmail.com>
2014-09-06 18:27:01 -0400

    Create Music.java

---
+++ Music.java
@@ -0,0 +1,33 @@
+package com.IHUNGRY;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class Music
+{
+private static MediaPlayer mp = null;
+public static boolean shouldPlay = true;
+
+	/**Stop old song and start new one. */
+	public static void play(Context context, int resource)
+	{
+		if (shouldPlay)
+		{
+			stop(context);
+			mp = MediaPlayer.create(context, resource);
+			mp.setLooping((true));
+			mp.start();
+		}
+	}
+	
+	/**Stop the music. */
+	public static void stop(Context context)
+	{
+		if (mp != null)
+		{
+			mp.stop();
+			mp.release();
+			mp = null;
+		}
+	}
+}
